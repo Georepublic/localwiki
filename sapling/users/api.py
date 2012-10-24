e08620fa0013a89ebb23f8703f6d57affd0579eb
Taro Matsuzawa <taro@georepublic.co.jp>
2012-10-25 06:03:31 +0900

    add custom API for customer

--- sapling/users/api.py
+++ sapling/users/api.py
@@ -1,4 +1,8 @@
-from tastypie.resources import ModelResource, ALL
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+from tastypie import fields
+from tastypie.models import ApiKey
+from tastypie.authorization import DjangoAuthorization
+from tastypie.authentication import ApiKeyAuthentication
 
 from django.contrib.auth.models import User
 from sapling.api import api
@@ -15,5 +19,28 @@ class UserResource(ModelResource):
             'date_joined': ALL,
         }
 
+class ApiKeyResource(ModelResource):
+    class Meta:
+        queryset = ApiKey.objects.all()
+        resource_name = "api_key"
+        authorization = DjangoAuthorization()
+        authentication = ApiKeyAuthentication()
+
+class UserWithApiKeyResource(ModelResource):
+    api_key = fields.ForeignKey('users.api.ApiKeyResource', 
+                                'api_key', null=True, readonly=True)
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'users_with_apikey'
+        fields = ['username', 'api_key', 'email']
+        filtering = {
+            'username': ALL,
+            'api_key': ALL,
+            'email': ALL,
+        }
+        authorization = DjangoAuthorization()
+        authentication = ApiKeyAuthentication()
 
 api.register(UserResource())
+api.register(ApiKeyResource())
+api.register(UserWithApiKeyResource())
